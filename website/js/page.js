198a6ee71500f972fb446b6d8b68125e8490dd9e
jiaxuanlyu <70524710+jiaxuanlyu@users.noreply.github.com>
2021-04-13 15:48:54 +0800

    upload draft of website

---
+++ website/js/page.js
@@ -0,0 +1,12 @@
+var map = L.map('map', {
+    center: [40.71505412378047, -73.94521497038218],
+    zoom: 14
+  });
+  
+  var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
+  }).addTo(map);
