a02aed66f51a715c43f41ab201bae9762cd5b4a6
Kate Sutton <42789282+kateesutt@users.noreply.github.com>
2021-04-14 17:21:49 -0400

    feature engineering with LION columns

--- rCode/model_0411.R
+++ rCode/model_0411.R
@@ -2,8 +2,7 @@ setwd('C:/Users/katee/Box Sync/Practicum/shp/')
 #avoid scientific notation
 options(scipen = 999)
 
-install.packages('ggcorrplot')
-install.packages('spdep')
+install.packages('visreg')
 
 #### packages ####
 library(tidyverse)
@@ -18,7 +17,8 @@ library(FNN)
 library(ggcorrplot)
 library(spdep)
 library(plotly)
-
+library(visreg)
+library(car)
 #palettes etc
 palette5 <- c("#25CB10", "#5AB60C", "#8FA108",   "#C48C04", "#FA7800")
 
@@ -45,6 +45,7 @@ Aug_pnts <- Aug_pnts %>%
 Aug_pnts <- Aug_pnts %>%
   filter(day <= 15)
 
+#might not need this
 borough<- st_read('https://data.cityofnewyork.us/resource/7t3b-ywvw.geojson')%>%
   st_transform(st_crs(Aug_pnts))
 
@@ -52,8 +53,16 @@ borough<- st_read('https://data.cityofnewyork.us/resource/7t3b-ywvw.geojson')%>%
 borough <- borough %>%
   filter(boro_name != "Staten Island")
 
-#find out pnts inside 2 boroughs
-Aug_pnts <- Aug_pnts[borough,]
+#haven't used this yet
+station<- st_read('Stations_Aug_2020.shp')%>%
+  st_transform(st_crs(Aug_pnts))
+
+#Station Extent
+extent <- st_read("station_extent_Aug2020.shp") %>%
+  st_transform(st_crs(Aug_pnts))
+
+#find out pnts inside citibike extent
+Aug_pnts <- Aug_pnts[extent,]
 
 
 #neighborhood DATA
@@ -61,16 +70,9 @@ Aug_pnts <- Aug_pnts[borough,]
 neighborhood <- st_read('https://data.cityofnewyork.us/resource/q2z5-ai38.geojson')%>%
   st_transform(st_crs(Aug_pnts))
 
-#mapview(neighborhood)
 
-#only neighboor in 4 boro
-neighborhood <- neighborhood[borough,]
-
-#Station Extent
-extent <- st_read("station_extent_Aug2020.shp") %>%
-  st_transform(st_crs(Aug_pnts))
-
-#mapview(station)
+#only neighboor in citibike extent
+neighborhood <- neighborhood[extent,]
 
 
 ##=====Roads and Bikelanes====##
@@ -78,28 +80,84 @@ extent <- st_read("station_extent_Aug2020.shp") %>%
 bike20d_buffer <- st_read("lion_buffer15/lion_buffer15.shp") %>%
   st_transform(st_crs(Aug_pnts))
 
-#select useful column
-bike20d_buffer <- bike20d_buffer %>%
-  select(Street, BikeLane, SegmentID, geometry)
-
-#only roads inside four boroughs
-#bike20d_buffer <- bike20d_buffer[borough,]
-
-# OR only roads inside citibike extent
+#clip roads to citibike extent
 bike20d_buffer<- bike20d_buffer[extent,]
 
-mapview(bike20d_buffer_2)
 
-#create a new column of whether it is a bikelane
+non_phys<- subset(bike20d_buffer, RW_TYPE==12)
+amtrak<-subset(bike20d_buffer, bike20d_buffer$Street=='AMTRAK-NORTHEAST LINE')
+
+#get rid of non-road data (eg shorelines and boundaries)
+bike20d_buffer_drop<- subset(bike20d_buffer, RW_TYPE !=12 &
+                               RW_TYPE != 7 &
+                               RW_TYPE != 14 &
+                               RW_TYPE != 2 &
+                               RW_TYPE != 4 &
+                               FeatureTyp != 1 &
+                               FeatureTyp != 2 &
+                               FeatureTyp != 3 &
+                               FeatureTyp != 7 &
+                               FeatureTyp != 8 &
+                               FeatureTyp != 9 &
+                               SegmentTyp !='E'&
+                               SegmentTyp !='G'&
+                               SegmentTyp !='F'&
+                               SegmentTyp !='T')
+
+#highways
+highways<- subset(bike20d_buffer, RW_TYPE==2 | SegmentTyp=='E')
+
+unique(highways$Street)
+
+mapview(highways)
+
+#divided streets
+divided<-subset(bike20d_buffer, SegmentTyp=='G' | SegmentTyp=='R' | SegmentTyp=='T' | SegmentTyp=='C')
+
+divided_better<-subset(bike20d_buffer, SegmentTyp=='R' | SegmentTyp=='T' |SegmentTyp=='C')
+
+mapview(divided_better)
+
+#tunnels
+tunnels<- subset(bike20d_buffer, RW_TYPE==4)
+mapview(tunnels)
+
+#path/trail
+trail<- subset(bike20d_buffer, RW_TYPE==6)
+mapview(trail)
+
+mapview(bike20d_buffer_drop)
+
+#add a highway, trail, and tunnel 
+bike20d_buffer_drop <- bike20d_buffer_drop %>% 
+  mutate(trailOrNot = case_when(
+    RW_TYPE == 6 ~ 1,
+    TRUE~0
+  ),
+  bikeLaneLv = case_when(
+    BikeLane == 1  ~ "Protected",
+    BikeLane == 2 | BikeLane == 4 | BikeLane == 5 | BikeLane == 8 | BikeLane == 9 | BikeLane == 10 ~ 'Unprotected', 
+    TRUE ~ 'None'
+  ),
+  bridgeOrNot = case_when(
+    RW_TYPE == 3 ~ 1,
+    TRUE~0
+  ))
+
+bike20d_buffer<- bike20d_buffer_drop
+
+#select useful column
 bike20d_buffer <- bike20d_buffer %>%
-  mutate(bikeline = ifelse(BikeLane == "1"| BikeLane == "2" | BikeLane == "5" | BikeLane == "9", "yes", "no"))
+  select(Street, BikeLane, SegmentID, FeatureTyp, SegmentTyp, NonPed,
+         TrafDir, SegCount, LBoro, XFrom, YFrom, RW_TYPE, Snow_Prior,
+         Number_Tra, Number_Par, Number_Tot, BIKE_TRAFD, 
+         StreetWidt, StreetWi_1, StreetWi_2, TRUCK_ROUT, POSTED_SPE, trailOrNot, 
+         bikeLaneLv, bridgeOrNot, geometry)
 
-#assign NA with no
-bike20d_buffer$bikeline[is.na(bike20d_buffer$bikeline)] <- "no"
+glimpse(bike20d_buffer)
 
 
-#other features
-
+# if we do dummy variables for each level of bikelane
 ##Protected bikelanes
 bike20d_buffer <- bike20d_buffer %>%
   mutate(protected = ifelse(BikeLane == "1", "yes", "no"))
@@ -113,6 +171,8 @@ bike20d_buffer <- bike20d_buffer %>%
 #assign NA with no
 bike20d_buffer$unprotected[is.na(bike20d_buffer$unprotected)] <- "no"
 
+
+#summing ridership onto streets
 #Find out the points inside bikelanes
 Aug.in <- Aug_pnts[bike20d_buffer,]
 
@@ -135,8 +195,9 @@ count.Aug <- ls.Aug %>%
 info.Aug <- merge(bike20d_buffer, count.Aug)
 
 #find out the number of trips on bikelanes
-bikeslines <- bike20d_buffer %>% filter(BikeLane == "1"| BikeLane == "2" | BikeLane == "5" | BikeLane == "9")
-bike.Aug <- merge(bikeslines, count.Aug)
+bikelanes <- bike20d_buffer %>% filter(BikeLane == "1"| BikeLane == "2" | BikeLane == "5" | BikeLane == "9" |
+                                         BikeLane == '4' | BikeLane == '8' | BikeLane == '10')
+bike.Aug <- merge(bikelanes, count.Aug)
 
 #find out the number of trips in each neighborhood
 bike.nh <- st_join(info.Aug, neighborhood %>% st_transform(st_crs(info.Aug)), join=st_intersects, left=TRUE)
@@ -177,7 +238,7 @@ nn_function <- function(measureFrom,measureTo,k) {
 info.Aug <- info.Aug %>% st_transform('ESRI:102711')
 bike20d_buffer <- bike20d_buffer %>% st_transform('ESRI:102711')
 bike.Aug <- bike.Aug %>% st_transform('ESRI:102711')
-bikeslines <- bikeslines %>% st_transform('ESRI:102711')
+bikelanes <- bikeslines %>% st_transform('ESRI:102711')
 bike.nh <- bike.nh %>% st_transform('ESRI:102711')
 
 
@@ -187,12 +248,12 @@ bike.nh <- bike.nh %>% st_transform('ESRI:102711')
 ###Distance to nearest bikelanes
 info.Aug <- info.Aug %>%
   mutate(dist.lane = nn_function(st_coordinates(st_centroid(info.Aug$geometry)),
-                                 st_coordinates(st_centroid(bikeslines$geometry)), 1))
+                                 st_coordinates(st_centroid(bikelanes$geometry)), 1))
 
 
 
-
-##Biketrips at cloest roads
+#### NOT DOING THIS RN ####
+##Biketrips at closest roads
 
 #function to get cloest idx
 nn_idx<- nn_function <- function(measureFrom,measureTo,k) {
@@ -240,12 +301,12 @@ for (i in 1:nrow(info.Aug)) {
 }
 
 
+#### INCLUDED THIS ####
 #The number of biketrips in each neighborhood
-
 info.Aug <- st_join(info.Aug, bike.nh, join=st_intersects, left=TRUE)
 
 
-
+#### NOT INCLUDED: C2 ####
 #the number of biketrips on the surrounding  (closest) 2 roads
 info.Aug2 <- info.Aug
 
@@ -275,11 +336,206 @@ info.Aug <- info.Aug %>%
 
 mapview(info.Aug)
 
-##====Regression====###
-#Make the regression
-reg <- lm(Count ~ bikeline + dist.lane + nhCount + C2, data=info.Aug)
 
-summary(reg)
+#### Adding more features ####
+
+#link_type
+info.Aug$link_type<- ifelse(grepl('street', info.Aug$Street, ignore.case=T), 'street',
+                            ifelse(grepl('avenue', info.Aug$Street, ignore.case=T), 'avenue',
+                                   ifelse(grepl('boulevard', info.Aug$Street, ignore.case=T), 'boulevard',
+                                          ifelse(grepl(' st ', info.Aug$Street, ignore.case = T), 'street', 
+                                                 ifelse(grepl(' ave ', info.Aug$Street, ignore.case = T), 'avenue', 'other')))))
+
+unique(info.Aug$link_type)
+
+info.Aug$isAve<- 0
+info.Aug$isAve[info.Aug$link_type=='avenue']<-1
+
+aves<-subset(info.Aug, link_type=='avenue')
+
+ggplot()+
+  geom_sf(data=aves)
+
+#borough
+boxplot(Count ~ LBoro,
+        data=info.Aug)
+
+info.Aug$isMH<-0
+info.Aug$isMH[info.Aug$LBoro==1]<-1
+
+info.Aug$Boro_F<-as.factor(info.Aug$LBoro)
+
+#travel lanes
+info.Aug$Number_Tra <- as.numeric(info.Aug$Number_Tra)
+info.Aug$Number_Tot <- as.numeric(info.Aug$Number_Tot)
+info.Aug$Number_Par <- as.numeric(info.Aug$Number_Par)
+
+glimpse(info.Aug)
+
+info.Aug$FewLanes<- 0
+info.Aug$FewLanes[info.Aug$Number_Tra>3]<-1
+
+#parked lanes
+mapview(info.Aug, zcol='Number_Par')
+
+park_4<-subset(info.Aug, Number_Par==4)
+mapview(park_4)
+
+#snow route
+critical<-subset(info.Aug, Snow_Prior=='C')
+mapview(critical)
+
+sector<-subset(info.Aug, Snow_Prior=='S')
+mapview(sector)
+
+haulster<-subset(info.Aug, Snow_Prior=='H')
+mapview(haulster)
+
+snow_v<-subset(info.Aug, Snow_Prior=='V')
+mapview(snow_v)
+
+info.Aug$SnowLv<-0
+info.Aug$SnowLv[info.Aug$Snow_Prior=='S']<-1
+
+#speed limit
+info.Aug$POSTED_SPE<-as.numeric(info.Aug$POSTED_SPE)
+ggplot(info.Aug, aes(x=POSTED_SPE, y=Count))+
+  geom_point(size=0.5, alpha=0.2)+
+  geom_smooth(method='lm')
+
+boxplot(Count~POSTED_SPE,
+        data=info.Aug)
+
+info.Aug$Speed_20s<-0
+info.Aug$Speed_20s[info.Aug$POSTED_SPE<30]<-1
+info.Aug$Speed_20s[info.Aug$POSTED_SPE==15]<-0
+
+#truck route
+info.Aug$Truck_Thru<-0
+info.Aug$Truck_Thru[info.Aug$TRUCK_ROUT==3]<-1
+
+
+#### Correlations ####
+#corplot of numeric variables against each other
+# create a correlation matrix of all numerical variables
+numericVars <- 
+  select_if(st_drop_geometry(info.Aug[, -c(1:2, 30)]), is.numeric) %>% 
+  na.omit()
+
+glimpse(numericVars)
+
+corr<-round(cor(numericVars), 1)
+p.mat<- cor_pmat(numericVars)
+
+ggcorrplot(corr, method='circle')
+
+ggcorrplot(corr, hc.order = TRUE, type='lower', insig='blank')
+
+#qualitative vars cor plot
+st_drop_geometry(info.Aug) %>% 
+  dplyr::select(Count, bikeLaneLv, POSTED_SPE, Number_Tra, Number_Tot,
+                Snow_Prior, TRUCK_ROUT, RW_TYPE, Number_Par, BIKE_TRAFD,
+                FeatureTyp, NonPed,
+                StreetWidt, trailOrNot, bridgeOrNot) %>%
+  gather(Variable, Value, -Count) %>% 
+  ggplot(aes(Value, Count)) +
+  geom_point(size = .5, shape=20, alpha = 0.5) + 
+  facet_wrap(~Variable, ncol = 3, scales = "free") +
+  labs(caption = "Count as a Function Some Variables")
+
+#boxplots
+boxplot(Count~bridgeOrNot,
+        data=info.Aug)
+
+boxplot(Count~bikeLaneLv,
+        data=info.Aug,
+        col='orange',
+        border='brown')
+
+boxplot(Count~POSTED_SPE,
+        data=info.Aug)
+
+boxplot(Count~StreetWidt,
+        data=info.Aug)
+
+boxplot(Count~Number_Tra,
+        data=info.Aug)
+
+ggplot()+
+  geom_point(data=info.Aug, aes(x=dist.lane, y=Count), alpha=0.2)+
+  geom_smooth(data= info.Aug, aes(x=dist.lane, y=Count), method='lm')
+
+ggplot()+
+  geom_point(data=info.Aug, aes(x=XFrom, y=Count), alpha=0.2)+
+  geom_smooth(data= info.Aug, aes(x=XFrom, y=Count), method='lm')
+
+#facet cor plot
+select_if(st_drop_geometry(info.Aug[, -c(1:2, 30)]), is.numeric) %>% 
+  na.omit()
+glimpse(info.Aug)
+correlation.long <-
+  st_drop_geometry(info.Aug) %>%
+  dplyr::select(-Street, -SegmentID) %>%
+  gather(Variable, Value, -Count)
+
+correlation.long <-
+  select_if(st_drop_geometry(info.Aug[, -c(1:2, 30)]), is.numeric) %>%
+  gather(Variable, Value, -Count)
+
+correlation.cor <-
+  correlation.long %>%
+  group_by(Variable) %>%
+  summarize(correlation = cor(Value, Count, use = "complete.obs"))
+
+
+ggplot(correlation.long, aes(Value, Count)) +
+  geom_point(size = 0.1) +
+  geom_text(data = correlation.cor, aes(label = paste("r =", round(correlation, 2))),
+            x=-Inf, y=Inf, vjust = 1.5, hjust = -.1) +
+  geom_smooth(method = "lm", se = FALSE, colour = "blue") +
+  facet_wrap(~Variable, ncol = 2, scales = "free") +
+  labs(title = "Count of trips as a function of some features")
+
+#map of some variables
+ggplot()+
+  geom_sf(data=info.Aug, aes(color=dist.lane))
+
+ggplot()+
+  geom_sf(data=info.Aug, aes(color=C2))
+
+ggplot()+
+  geom_sf(data=info.Aug, aes(color=nhCount))
+
+####====Regression====####
+glimpse(info.Aug)
+
+info.Aug$Centroid <- st_centroid(info.Aug$geometry)
+
+st_coordinates(info.Aug$Centroid)
+
+#Make the regression
+reg_b <- lm(Count ~ bikeLaneLv + dist.lane + Number_Tra + StreetWidt + SnowLv + POSTED_SPE  + TRUCK_ROUT
+            + YFrom*isMH + XFrom*isMH + nhCount, data=info.Aug)
+summary(reg_b)
+
+reg_p <- lm(Count ~ bikeLaneLv + dist.lane + Number_Tra + StreetWidt + SnowLv + POSTED_SPE  + TRUCK_ROUT
+             + YFrom*isMH + XFrom*isMH + nhCount , data=info.Aug)
+summary(reg_p)
+
+reg_p2<-lm(Count ~ bikeLaneLv + isMH + dist.lane + Number_Tra + StreetWidt + SnowLv + POSTED_SPE + TRUCK_ROUT , data=info.Aug)
+summary(reg_p2)
+
+reg_2 <- lm(Count ~ bikeLaneLv + LBoro + dist.lane + Number_Tra + StreetWidt +
+              XFrom + YFrom + nhCount + trailOrNot + Snow_Prior + TRUCK_ROUT 
+            + NonPed + link_type, data=info.Aug)
+
+summary(reg_2)
+
+reg_3 <- lm(Count ~ bikeLaneLv + isMH + dist.lane + Number_Tra + StreetWidt +
+              XFrom + YFrom + nhCount + trailOrNot + Snow_Prior + TRUCK_ROUT 
+            + NonPed + link_type, data=info.Aug)
+
+summary(reg_3)
 
 stargazer(reg, type = "html",
           title = "Regression results",
@@ -301,6 +557,9 @@ ggplot()+
 ggplot()+
   geom_sf(data=info.Aug, aes(color=res_reg1))
 
+info.Aug$res_reg2 <- residuals(reg_2)
+info.Aug$fitted_reg2<- fitted(reg_2)
+
 #select only those with higher value residuals
 high_err<- subset(info.Aug, res_reg1>10)
 
@@ -318,39 +577,7 @@ hist(info.Aug$res_reg1, col='skyblue3', breaks = 50, main='Histogram of Residual
 #### more detailed modeling - train/test and errors ####
 
 
-#corplot of numeric variables against each other
-# create a correlation matrix of all numerical variables
-numericVars <- 
-  select_if(st_drop_geometry(info.Aug[, -c(1:2, 5)]), is.numeric) %>% 
-  na.omit()
 
-glimpse(numericVars)
-
-corr<-round(cor(numericVars), 1)
-p.mat<- cor_pmat(numericVars)
-
-ggcorrplot(corr, method='circle')
-
-ggcorrplot(corr, hc.order = TRUE, type='lower', insig='blank')
-
-#correlation plots against dependent var
-st_drop_geometry(info.Aug) %>% 
-  dplyr::select(Count, bikeline, BikeLane, dist.lane, C2, nhCount) %>%
-  gather(Variable, Value, -Count) %>% 
-  ggplot(aes(Value, Count)) +
-  geom_point(size = .5) + geom_smooth(method = "lm", se=F, colour = "#FA7800") +
-  facet_wrap(~Variable, ncol = 3, scales = "free") +
-  labs(caption = "Count as a Function Some Numerical Variables")
-
-#map of some variables
-ggplot()+
-  geom_sf(data=info.Aug, aes(color=dist.lane))
-
-ggplot()+
-  geom_sf(data=info.Aug, aes(color=C2))
-
-ggplot()+
-  geom_sf(data=info.Aug, aes(color=nhCount))
 
 #retrain model into training and test sets
 # create the training set
@@ -483,8 +710,8 @@ ggplotly(count_lag_plot)
 error_lag_plot<- ggplot(test, aes(x=lagCountError, y=Count, label=Street)) +
   geom_point(colour = "#FA7800") +
   geom_smooth(method = "lm", se = FALSE, colour = "#25CB10") +
-  labs(title = "Error as a function of the spatial lag of count",
-       caption = "Error as a function of the spatial lag of count",
+  labs(title = "Count as a function of the spatial lag of error",
+       caption = "Count as a function of the spatial lag of error",
        x = "Spatial lag of errors (Mean error of 5 nearest neighbors)",
        y = "Trip Count")
 error_lag_plot
@@ -503,14 +730,27 @@ boroughs_clip<-st_intersection(borough, extent)
 
 ggplot()+
   geom_sf(data=boroughs_clip, fill='white', color='white')+
-  geom_sf(data=high_err_test, aes(color=Count.Error), size=1.2)+
-  geom_sf(data=high_err_train, aes(color=Count.Error), size =1.2)+
+  geom_sf(data=high_err_test, aes(color=Count.Error), size=0.7)+
+  geom_sf(data=high_err_train, aes(color=Count.Error), size =0.7)+
   scale_color_continuous(type='viridis')+
   labs(title='Residuals Larger than 100')
 
 ggplot()+
   geom_sf(data=boroughs_clip, fill='white', color='white')+
-  geom_sf(data=med_err_test, aes(color=Count.Error), size=1.2)+
-  geom_sf(data=med_err_train, aes(color=Count.Error), size =1.2)+
+  geom_sf(data=med_err_test, aes(color=Count.Error), size=0.7)+
+  geom_sf(data=med_err_train, aes(color=Count.Error), size =0.7)+
   scale_color_continuous(type='viridis')+
   labs(title='Residuals Larger than 10')
+
+ggplot()+
+  geom_sf(data=boroughs_clip, fill='white', color='white')+
+  geom_sf(data=info.Aug, aes(color=res_reg1), size=0.5)+
+  scale_color_continuous(type='viridis')+
+  labs(title='All Residuals')
+
+
+glimpse(info.Aug)
+
+
+# maps of filtering data
+
